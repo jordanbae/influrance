76ad00591437fc7cd60196e7da8845b5dbb95dee
jordanbae <laphon.nala@bumail.net>
2023-01-15 14:14:53 +0700

    add mock people image

--- src/utils/data.js
+++ src/utils/data.js
@@ -13,63 +13,63 @@ export const comments = [
     post: "Twitch Streamer",
     comment:
       "I highly recommend Influrance's influencer insurance service to all influencers, it gives me peace of mind",
-    img: "./people1.png",
+    img: "./person1.png",
   },
   {
     name: "Jiraporn Jamsai",
     post: "Content Creator",
     comment:
       "I've been using Influrance's insurance service for a while now and I'm very satisfied with it",
-    img: "./people2.png",
+    img: "./person2.png",
   },
   {
     name: "Wipawee Jaisamut",
     post: "Content Creator",
     comment:
       "The coverage options are great and the customer service is excellent, I'm happy with Influrance's influencer insurance",
-    img: "./people3.png",
+    img: "./person3.png",
   },
   {
     name: "Waraporn Suwannakit",
     post: "Twitch Streamer",
     comment:
       "I never thought I would need insurance as an influencer, but Influrance proved me wrong, it's worth every penny",
-    img: "./people4.png",
+    img: "./person4.png",
   },
   {
     name: "Thanwarat Boonchuay",
     post: "Content Creator",
     comment:
       "I trust Influrance to protect my income as an influencer, the insurance service is very reliable",
-    img: "./people5.png",
+    img: "./person5.png",
   },
   {
     name: "Supaporn Chantarapitak",
     post: "Content Creator",
     comment:
       "I'm glad I found Influrance's influencer insurance, it gives me peace of mind knowing I'm covered",
-    img: "./people6.png",
+    img: "./person6.png",
   },
   {
     name: "Kanyarat Supakijyanon",
     post: "Twitch Streamer",
     comment:
       "I highly recommend Influrance's influencer insurance service, it's affordable and comprehensive",
-    img: "./people7.png",
+    img: "./person7.png",
   },
   {
     name: "Jintana Wong",
     post: "Content Creator",
     comment:
       "I never thought I needed an insurance as influencer but after using Influrance's service, I realized how important it is, it's comprehensive and affordable",
-    img: "./people8.png",
+    img: "./person1.png",
   },
   {
     name: "Thanwarat Leesangchai",
     post: "Twitch Streamer",
     comment:
       "I'm so happy I found Influrance insurance service for influencers, it gives me a peace of mind knowing I'm covered",
-    img: "./people9.png",
+    img: "./person2.png",
   },
 ];
 
