53e6cc6a4943a9508b4db1c8a626d78d4c027829
jordanbae <laphon.nala@bumail.net>
2023-01-15 13:58:01 +0700

    Init vite-react-scss, framer-motion, raw-mock-data

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>App.jsx</h1>;
+};
+
+export default App;
---
+++ src/utils/data.js
@@ -0,0 +1,112 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+
+export const WhatWeDo = [
+  "At Influrance, we understand the unique challenges that influencers face when it comes to protecting their income and reputation online. That's why we've created a comprehensive insurance solution specifically designed for influencers. Our insurance packages offer coverage for brand partnerships, content creation, income interruption, and online defamation, providing influencers with the peace of mind they need to focus on what they do best - creating content and engaging with their followers.",
+  "Whether you're an up-and-coming influencer just starting out or an established influencer with a large following, we can help you protect your income and reputation. Our flexible coverage options and competitive pricing make it easy to find the right package for your needs. So, if you're an influencer looking for protection, look no further than Influrance.",
+];
+
+export const comments = [
+  {
+    name: "Narisa Somsri",
+    post: "Twitch Streamer",
+    comment:
+      "I highly recommend Influrance's influencer insurance service to all influencers, it gives me peace of mind",
+    img: "./people1.png",
+  },
+  {
+    name: "Jiraporn Jamsai",
+    post: "Content Creator",
+    comment:
+      "I've been using Influrance's insurance service for a while now and I'm very satisfied with it",
+    img: "./people2.png",
+  },
+  {
+    name: "Wipawee Jaisamut",
+    post: "Content Creator",
+    comment:
+      "The coverage options are great and the customer service is excellent, I'm happy with Influrance's influencer insurance",
+    img: "./people3.png",
+  },
+  {
+    name: "Waraporn Suwannakit",
+    post: "Twitch Streamer",
+    comment:
+      "I never thought I would need insurance as an influencer, but Influrance proved me wrong, it's worth every penny",
+    img: "./people4.png",
+  },
+  {
+    name: "Thanwarat Boonchuay",
+    post: "Content Creator",
+    comment:
+      "I trust Influrance to protect my income as an influencer, the insurance service is very reliable",
+    img: "./people5.png",
+  },
+  {
+    name: "Supaporn Chantarapitak",
+    post: "Content Creator",
+    comment:
+      "I'm glad I found Influrance's influencer insurance, it gives me peace of mind knowing I'm covered",
+    img: "./people6.png",
+  },
+  {
+    name: "Kanyarat Supakijyanon",
+    post: "Twitch Streamer",
+    comment:
+      "I highly recommend Influrance's influencer insurance service, it's affordable and comprehensive",
+    img: "./people7.png",
+  },
+  {
+    name: "Jintana Wong",
+    post: "Content Creator",
+    comment:
+      "I never thought I needed an insurance as influencer but after using Influrance's service, I realized how important it is, it's comprehensive and affordable",
+    img: "./people8.png",
+  },
+  {
+    name: "Thanwarat Leesangchai",
+    post: "Twitch Streamer",
+    comment:
+      "I'm so happy I found Influrance insurance service for influencers, it gives me a peace of mind knowing I'm covered",
+    img: "./people9.png",
+  },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
