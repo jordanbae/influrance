1dc66939ddcb7edf6e087977d97a07ad9751f514
jordanbae <laphon.nala@bumail.net>
2023-01-20 01:59:19 +0700

    removed old files for refactoring

--- client/src/components/PackagesForm/Tier2.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import style from "./Forms.module.scss";
-const Tier2 = () => {
-  return <div className={style.container}>Tier 2</div>;
-};
-
-export default Tier2;
--- client/src/components/PackagesForm/Tier4.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import style from "./Forms.module.scss";
-const Tier4 = () => {
-  return <div className={style.container}>Tier 4</div>;
-};
-
-export default Tier4;
